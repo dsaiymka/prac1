9322092f7db20fe7dafc872553822dd0ea671801
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:25:50 +0500

    fcvsrvrs

---
+++ prac2/if_elif_else.py
@@ -0,0 +1,44 @@
+#1
+score = 95
+if score >= 90:
+    print("Отлично")
+elif score >= 60:
+    print("Хорошо")
+else:
+    print("Плохо")
+
+#2
+temperature = 10
+if temperature > 25:
+    print("Жарко")
+elif temperature >= 15:
+    print("Тепло")
+else:
+    print("Холодно")
+
+#3
+age = 20
+if age < 13:
+    print("Ребёнок")
+elif age < 18:
+    print("Подросток")
+else:
+    print("Взрослый")
+
+#4
+x = 0
+if x > 0:
+    print("Положительное")
+elif x < 0:
+    print("Отрицательное")
+else:
+    print("Ноль")
+
+#5
+grade = 'B'
+if grade == 'A':
+    print("Отлично")
+elif grade == 'B':
+    print("Хорошо")
+else:
+    print("Попробуй снова")
