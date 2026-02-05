fae64cf9c4182b5f2b02eb0a909e741334b13dd7
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:17:22 +0500

    gbsfv

---
+++ prac2/boolean_operators.py
@@ -0,0 +1,23 @@
+#1
+a = True
+b = False
+print(a and b)
+
+#2
+x = 5
+y = 10
+print(x < y or x > y)
+
+#3
+is_sunny = True
+is_warm = False
+print(not is_sunny)
+
+#4
+num = 7
+print(num > 0 and num < 10)
+
+#5
+val1 = False
+val2 = False
+print(val1 or val2)
