dd0e0a7645a20280f882ba9dd70026a5ad95469e
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:13:59 +0500

    tvsg

---
+++ prac2/boolean_comparison.py
@@ -0,0 +1,31 @@
+#1
+a = 5
+b = 3
+print(a > b) 
+print(a < b) 
+
+#2
+name1 = "Alice"
+name2 = "Bob"
+print(name1 == "Alice") 
+print(name1 != name2)
+
+#3
+x = 10
+y = 10
+z = 7
+print(x == y)
+print(x != z)
+
+#4
+a = 5
+b = 10
+c = 7
+print(a < b and c < b)
+print(a > b or c < b)
+
+#5
+is_sunny = True
+is_raining = False
+print(is_sunny == True) 
+print(is_raining != True) 
