77360799f5f21aa88e28b3f50ca496e235beff28
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:32:48 +0500

    vav

---
+++ prac2/while_continue.py
@@ -0,0 +1,39 @@
+#1
+i = 0
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+#2
+num = 0
+while num < 10:
+    num += 1
+    if num % 2 == 0:
+        continue
+    print(num)
+
+#3
+n = 0
+while n < 5:
+    n += 1
+    if n == 2 or n == 4:
+        continue
+    print(n)
+
+#4
+i = 0
+while i < 7:
+    i += 1
+    if i % 3 == 0:
+        continue
+    print(i)
+
+#5
+count = 0
+while count < 6:
+    count += 1
+    if count == 1 or count == 5:
+        continue
+    print(count)
