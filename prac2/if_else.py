28edb4d6ce872af09cef6126d6dfa2e37c14b881
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:23:56 +0500

    fvercver

---
+++ prac2/if_else.py
@@ -0,0 +1,34 @@
+#1
+x = 10
+if x > 5:
+    print("x больше 5")
+else:
+    print("x меньше или равно 5")
+
+#2
+y = 3
+if y % 2 == 0:
+    print("y чётное")
+else:
+    print("y нечётное")
+
+#3
+age = 17
+if age >= 18:
+    print("Взрослый")
+else:
+    print("Несовершеннолетний")
+
+#4
+score = 45
+if score >= 60:
+    print("Сдал")
+else:
+    print("Не сдал")
+
+#5
+temperature = 20
+if temperature > 25:
+    print("Жарко")
+else:
+    print("Не жарко")
