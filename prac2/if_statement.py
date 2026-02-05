da555d6dff0a2f5dbaedc059b8689b1b8eb08080
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:21:40 +0500

    bsdgtG

---
+++ prac2/if_statement.py
@@ -0,0 +1,36 @@
+#1
+x = 10
+if x > 5:
+    print("x больше 5")
+
+#2
+y = 3
+if y % 2 == 0:
+    print("y чётное")
+else:
+    print("y нечётное")
+
+#3
+age = 18
+if age >= 18:
+    print("Взрослый")
+else:
+    print("Несовершеннолетний")
+
+#4
+score = 75
+if score >= 90:
+    print("Отлично")
+elif score >= 60:
+    print("Хорошо")
+else:
+    print("Плохо")
+
+#5
+temperature = 30
+if temperature > 25:
+    print("Жарко")
+elif temperature < 15:
+    print("Холодно")
+else:
+    print("Комфортно")
