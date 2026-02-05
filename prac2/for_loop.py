875a19ca4b16f015c224c6d7996dab48618cc699
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:35:26 +0500

    VRV

---
+++ prac2/for_loop.py
@@ -0,0 +1,24 @@
+#1
+for i in range(5):
+    print(i)
+
+#2
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#3
+for i in range(1, 6):
+    print("Квадрат числа", i, "=", i**2)
+
+#4
+name = "Alice"
+for letter in name:
+    print(letter)
+
+#5
+numbers = [2, 4, 6, 8]
+sum_numbers = 0
+for num in numbers:
+    sum_numbers += num
+print("Сумма чисел:", sum_numbers)
