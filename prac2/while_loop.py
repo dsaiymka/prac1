2c562dcea923d40fbb2a75d0404a086c9735e83c
DostaiAiym <dsaiymka@gmail.com>
2026-02-05 23:29:28 +0500

    vavrf

---
+++ prac2/while_loop.py
@@ -0,0 +1,29 @@
+#1
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+#2
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+
+#3
+n = 0
+while n < 3:
+    print("Привет")
+    n += 1
+
+#4
+num = 2
+while num <= 16:
+    print(num)
+    num *= 2
+
+#5
+text = ""
+while text != "stop":
+    text = input("Введите текст (для выхода напишите 'stop'): ")
+    print("Вы ввели:", text)
