9c858bcec315d8fddcd97db7637af0bab6b90fff
dsaiymka <dsaiymka@gmail.com>
2026-04-03 02:28:22 +0500

    Wrvfw

---
+++ prac6/file_handling/write_files.py
@@ -0,0 +1,6 @@
+# Create and write to a file
+with open("sample.txt", "w") as f:
+    f.write("Hello, this is line 1\n")
+    f.write("This is line 2\n")
+
+print("File created and written successfully.")
